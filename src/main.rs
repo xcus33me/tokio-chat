00b776230be9ebc9e7c3b7e332b2b3b078703b2e
xcus33me <HarvieOrigin@yandex.ru>
2024-07-26 15:39:30 +0300

    added tokio::spawn

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use tokio::{
-    io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader}, 
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader}, 
     net::TcpListener
 };
 
@@ -7,15 +7,23 @@ use tokio::{
 async fn main() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
 
-    let (mut socket, _addr) = listener.accept().await.unwrap();
-
     loop {
-        let mut reader = BufReader::new(&mut socket);
+        let (mut socket, _addr) = listener.accept().await.unwrap();
 
+        tokio::spawn(async move {
+        let (reader, mut writer) = socket.split();
+
+        let mut reader = BufReader::new(reader);
         let mut line = String::new();
-        
-        let bytes_read = reader.read_line(&mut line).await.unwrap();
-    
-        socket.write_all(line.as_bytes()).await.unwrap();
+
+        loop {
+            let bytes_read = reader.read_line(&mut line).await.unwrap();
+            if bytes_read == 0 {
+                break;
+            }
+
+            writer.write_all(line.as_bytes()).await.unwrap();
+            line.clear();
+        }});
     }
 }
