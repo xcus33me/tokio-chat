7974d9b4da5def3c38949fbfd1374488ad8fa391
xcus33me <HarvieOrigin@yandex.ru>
2024-07-26 15:15:25 +0300

    echo server

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,21 @@
+use tokio::{
+    io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader}, 
+    net::TcpListener
+};
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("localhost:8080").await.unwrap();
+
+    let (mut socket, _addr) = listener.accept().await.unwrap();
+
+    loop {
+        let mut reader = BufReader::new(&mut socket);
+
+        let mut line = String::new();
+        
+        let bytes_read = reader.read_line(&mut line).await.unwrap();
+    
+        socket.write_all(line.as_bytes()).await.unwrap();
+    }
 }
